ac7800f9b552feda925a597af8b09cc83bc4d678
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-12-13 16:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function isSameType(value1, value2) {
-  //your js code here
+let value1 = prompt('Enter a value1')
+let value2 = prompt('Enter a value2')
+if(typeof value1 == typeof value2){
+ console.log("True");
 }
-
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+else{
+	console.log("False");
+}
