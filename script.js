0cc52ba7a093e2266b199bf1304fc8e3473bb891
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-12-13 16:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let value1 = prompt('Enter a value1')
 let value2 = prompt('Enter a value2')
-if(typeof value1 == typeof value2){
+if(typeof value1 === typeof value2){
  console.log("True");
 }
 else{
-	console.log("False");
+console.log("False");
 }
